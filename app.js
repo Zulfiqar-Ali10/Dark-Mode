13dfb5c8db80fa0faca9757fb7d6327050eee129
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-10-28 16:40:22 +0500

    Dark-mode

---
+++ app.js
@@ -0,0 +1,32 @@
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+  
+  
+////////////////////////// darkmode.js animation ////////////////
+
+//   const options = {
+    //     bottom: '64px', // default: '32px'
+    //     right: 'unset', // default: '32px'
+    //     left: '32px', // default: 'unset'
+    //     time: '0.5s', // default: '0.3s'
+    //     mixColor: '#fff', // default: '#fff'
+    //     backgroundColor: '#fff',  // default: '#fff'
+    //     buttonColorDark: '#100f2c',  // default: '#100f2c'
+    //     buttonColorLight: '#fff', // default: '#fff'
+    //     saveInCookies: false, // default: true,
+    //     label: 'Dark', // default: ''
+    //     autoMatchOsTheme: true // default: true
+    //   }
+    
+    //   const darkmode = new Darkmode(options);
+    //   darkmode.showWidget();
+    
+    ////////////////////////// darkmode.js animation ////////////////
+
+////////////////////////animation.....................
+
+AOS.init();
+
+////////////////////////animation.....................
